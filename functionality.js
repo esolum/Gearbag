2bbb71a4110e9c09c1e1e550c4e2aa35a3563cf9
esolum <erica.solum@yahoo.com>
2014-02-24 15:55:09 -0800

    All dat javascript goodness

---
+++ functionality.js
@@ -0,0 +1,29 @@
+/* All my javascript backend */
+
+{
+	var sqlDB = new MooSQL({
+        	//Database name
+        	dbName:'Gearbag',
+        	
+        	//Database version (max 4 numbers seperated by dots)
+        	dbVersion:'1.0',
+        
+        	//Database description (officially database display name)
+        	dbDesc:'This is a gearbag database',
+        	
+        	//Estimated size
+        	dbSize:20*100
+    		});
+	
+	/* Trigger stuff to happen if errors pop up */
+	
+	
+	
+	function Device(){
+		deviceName: {},
+		deviceImage: {}
+		
+	}
+	
+
+}
