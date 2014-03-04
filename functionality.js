a13404612ae428b9d90e6aaaba4e00674229c2a7
esolum <erica.solum@yahoo.com>
2014-03-04 01:08:13 -0800

    Gets list of devices

--- functionality.js
+++ functionality.js
@@ -1,6 +1,11 @@
 /* All my javascript backend */
-
+/* Using API 0.1 because screw 2.0 its hard */
+/* Wait jk lol I win */
 {
+	var offset = 0;
+    var limit = 20;
+    var data;
+    var arr;
 	var sqlDB = new MooSQL({
         	//Database name
         	dbName:'Gearbag',
@@ -14,16 +19,78 @@
         	//Estimated size
         	dbSize:20*100
     		});
+    
+    window.addEvent('domready', function() {
+    			
+    			//alert('The DOM is ready!');
+    			// Do some stuff
+    			
+    			sqlDB.addEvent('notSupported', function(){
+    				console.log("Sorry, but the Gearbag feature does not work with your browser. Go download Chrome.");
+    			});
+    			sqlDB.addEvent('databaseCreated', function(){
+    				alert('Created.');
+    			});
+    			
+    			
+    			loadDevices();
+    			
+    			
+
+    }); //End of window event
+    function loadDevices(){
+    	
+    	var devReq = new Request({
+			url: 'https://www.ifixit.com/api/2.0/categories/all',
+			callbackKey: 'reqInfo',
+			onRequest: function(url){
+				
+				// a script tag is created with a src attribute equal to url
+			},
+			onComplete: function(data){
+				arr = data.split(",");
+				var i=0;
+    			while(i<20){
+    				console.log(arr[i]);
+    				i++;
+    			}
+			}
+			
+		}).get(data);
+    } // End of loadDevices()			
+	function printArray(array, size){
+		for(var i=0; i<size; i++){
+			console.log(array[i]);
+		}
+	}		 
 	
 	/* Trigger stuff to happen if errors pop up */
 	
 	
 	
-	function Device(){
-		deviceName: {},
-		deviceImage: {}
-		
-	}
+	
+	
+	
+	
+	//make an item 
+	/*function makeItem(name, image){
+		var item = new Element('div'){
+			class: 'item',
+			}
+		}
+	}*/
+	
 	
 
 }
+
+
+/* STUBS   -   FUTURE STUFF */
+
+//addDevice()
+
+
+
+
+
+
