e5d4e0b1f5f4017b6cd50df9008199408c184994
esolum <erica.solum@yahoo.com>
2014-03-06 11:43:54 -0800

    Update functionality.js

--- functionality.js
+++ functionality.js
@@ -5,7 +5,8 @@
 	var offset = 0;
     var limit = 20;
     var data;
-    var arr;
+    
+    var devNames = new Array();
 	var sqlDB = new MooSQL({
         	//Database name
         	dbName:'Gearbag',
@@ -20,6 +21,36 @@
         	dbSize:20*100
     		});
     
+     function loadDevices(){
+    	//request to get the list of "categories"
+    	var devReq = new Request({
+			url: 'https://www.ifixit.com/api/2.0/categories/all?limit=32&offset=0',
+			callbackKey: 'reqInfo',
+			onRequest: function(url){
+				
+				// a script tag is created with a src attribute equal to url
+			},
+			onFailure: function(){
+				console.log("Failed to complete request.");
+			},
+			onComplete: function(data){
+				
+				data = data.substr(1, data.length-2); //remove brackets
+				
+				data = data.split(',');
+				
+				for(var i=0; i<data.length; i++)
+				{
+					
+					var imageTag= getImage(data[i], i);
+					
+				}
+				
+			
+			}
+			
+		}).get(data);
+    }
     window.addEvent('domready', function() {
     			
     			//alert('The DOM is ready!');
@@ -38,57 +69,77 @@
     			
 
     }); //End of window event
-    function loadDevices(){
-    	
-    	var devReq = new Request({
-			url: 'https://www.ifixit.com/api/2.0/categories/all',
-			callbackKey: 'reqInfo',
-			onRequest: function(url){
-				
-				// a script tag is created with a src attribute equal to url
-			},
-			onComplete: function(data){
-				arr = data.split(",");
-				var i=0;
-    			while(i<20){
-    				console.log(arr[i]);
-    				i++;
-    			}
-			}
-			
-		}).get(data);
-    } // End of loadDevices()			
+    // End of loadDevices()			
 	function printArray(array, size){
 		for(var i=0; i<size; i++){
 			console.log(array[i]);
 		}
 	}		 
 	
-	/* Trigger stuff to happen if errors pop up */
+	function createDevice(name, imgTag, displayNum){
+		
+		
+		var option = new Element('img');
+		option.src = imgTag;
+		option.margin = '10px';
+		option.class = 'device-column';
+		option.display = 'inline';
+		if(displayNum < 7) option.inject('column1');
+		else if(displayNum >= 7 && displayNum< 13) option.inject('column2');
+		else if(displayNum >= 13 && displayNum<19 ) option.inject('column3');
+		else if(displayNum >= 19 && displayNum<25 ) option.inject('column4');
+		else if(displayNum >= 25 && displayNum<35 ) option.inject('column5');
+		
+	}
 	
-	
-	
-	
-	
-	
-	
-	//make an item 
-	/*function makeItem(name, image){
-		var item = new Element('div'){
-			class: 'item',
+	function createURL(nameString){
+		var nameArray = nameString.split(' ');
+		var urlName = nameArray.join('_');
+
+		urlName = urlName.substr(1, urlName.length-2);
+		
+		return urlName;	
+		
+	}
+	function getImage(name, displayNum){
+		var devName = name;
+		
+		name = createURL(name);
+		var imageReq = new Request({
+			url: 'https://www.ifixit.com/api/2.0/categories/' + name,
+			callbackKey: 'reqInfo',
+			onRequest: function(url){
+				
+				// a script tag is created with a src attribute equal to url
+			},
+			onComplete: function(data){
+				
+				var device = JSON.parse(data);
+				var imgTag = device.image.thumbnail;
+				//if(stuff.image != null) console.log(stuff.image.thumbnail);
+				
+				//Got the image url, now insert it into the page
+				createDevice(devName, imgTag, displayNum);
+			
 			}
-		}
-	}*/
+			
+		}).get(data);
+	}
 	
 	
 
 }
 
 
-/* STUBS   -   FUTURE STUFF */
+/* FUTURE STUFF */
 
-//addDevice()
+/*
+	Create the table that will store all the devices
+	Add each device to the table
+	Nix the next and previous buttons and implement a scroller or something instead
+	Add a search element
 
+*/
 
 
 
